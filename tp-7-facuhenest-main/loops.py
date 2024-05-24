a66c77e5c5d4d700ab19857481e11897259ba6da
facuhenest <162722573+facuhenest@users.noreply.github.com>
2024-05-23 23:51:42 -0300

    Add files via upload

--- tp-7-facuhenest-main/loops.py
+++ tp-7-facuhenest-main/loops.py
@@ -1,45 +1,73 @@
-def index_of_by_index(word, list, index):
-    index = index
-    while index < len(list):
-        if word == list[index]:
+def index_of_by_index(word, strings, start_index):
+    index = start_index
+    while index < len(strings):
+        if word == strings[index]:
             return index
         index += 1 
-    return -1 
+    return -1
 
-def index_of_empty(list):
-    index = 0
-    while index < len(list):
-        if list[index] == "":
-            return index
-        index += 1 
-    return -1   
+colors = ["Red", "Green", "White", "Black", "Pink", "Yellow", "Black"]
+print(index_of_by_index("Black", colors, 1))  
+print(index_of_by_index("Black", colors, 4)) 
+print(index_of_by_index("Green", colors, 2))  
 
-def index_of(word, list):
+
+def index_of_empty(strings):
     index = 0
-    while index < len(list):
-        if word == list[index]:
+    while index < len(strings):
+        if strings[index] == "":
             return index
         index += 1 
-    return -1   
-def put(word, list):
+    return -1
+
+colors = ["Red", "Green", "White", "Black", "Pink", "Yellow", "Black"]
+print(index_of_empty(colors)) 
+
+colors = ["Red", "Green", "", "", "Pink", "", "Black"]
+print(index_of_empty(colors))  
+
+def index_of(word, strings):
     index = 0
-    while index < len(list):
-        if list[index] == "":
-            list[index] = word
+    while index < len(strings):
+        if word == strings[index]:
             return index
         index += 1
     return -1
 
+colors = ["Red", "Green", "White", "Black", "Pink", "Yellow", "Black"]
+print(index_of("Black", colors))  
+print(index_of("Blue", colors))  
+ 
+
+def put(word, strings):
+    index = 0
+    while index < len(strings):
+        if strings[index] == "":
+            strings[index] = word
+            return index
+        index += 1
+    return -1
+
+colors = ["Red", "Green", "", "", "Pink", "", "Black"]
+print(put("Blue", colors))  
+
+colors = ["Red", "Green", "White", "Black", "Pink", "Yellow", "Black"]
+print(put("Blue", colors))  
 
 
 
-def remove(word, list):
+def remove(word, strings):
     index = 0
     eliminations = 0
-    while index < len(list):
-        if word == list[index]:
-            list[index] = ""
+    while index < len(strings):
+        if word == strings[index]:
+            strings[index] = ""
             eliminations += 1
         index += 1
     return eliminations
+
+colors = ["Red", "Green", "White", "Black", "Pink", "Yellow", "Black"]
+print(remove("Black", colors)) 
+print(remove("Blue", colors))  
+
 
--- tp-7-facuhenest-main/loops_and_print.py
+++ tp-7-facuhenest-main/loops_and_print.py
@@ -1,19 +1,21 @@
-def enumerate_list(list):
+def enumerate_list(strings):
     new_list = []
-    index_new = 0
-    for current_elem in list:
-        if current_elem != "":
-            new_list.append(f"{index_new}. {current_elem}")
-            index_new += 1
+    for index, current_elem in enumerate(strings):
+        if current_elem:
+            new_list.append(f"{index}. {current_elem}")
     return new_list
 
+colors = ["Red", "Green", "", "White", "Black"]
+print(enumerate_list(colors))  
 
 
-def enumerate_backwards(list):
+
+def enumerate_backwards(strings):
     new_list = []
-    index_new = 0
-    for current_elem in list:
-        if current_elem != "":
-            new_list.append(f"{index_new}. {current_elem[::-1]}")
-            index_new += 1
+    for index, current_elem in enumerate(strings):
+        if current_elem:
+            new_list.append(f"{index}. {current_elem[::-1]}")
     return new_list
+
+colors = ["Red", "Green", "", "White", "Black"]
+print(enumerate_backwards(colors))  
