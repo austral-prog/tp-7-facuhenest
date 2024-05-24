fff8dec9d3a82036cb678cdf6039dbdc825bea18
facuhenest <162722573+facuhenest@users.noreply.github.com>
2024-05-24 11:17:31 -0300

    Add files via upload

--- loops.py
+++ loops.py
@@ -1,18 +1,44 @@
 def index_of_by_index(word, list, index):
-    return -1
-
+    index = index
+    while index < len(list):
+        if word == list[index]:
+            return index
+        index += 1 
+    return -1 
 
 def index_of_empty(list):
-    return -1
-
+    index = 0
+    while index < len(list):
+        if list[index] == "":
+            return index
+        index += 1 
+    return -1   
 
 def index_of(word, list):
-    return -1
-
-
+    index = 0
+    while index < len(list):
+        if word == list[index]:
+            return index
+        index += 1 
+    return -1   
 def put(word, list):
+    index = 0
+    while index < len(list):
+        if list[index] == "":
+            list[index] = word
+            return index
+        index += 1
     return -1
+
+
 
 
 def remove(word, list):
-    return -1
+    index = 0
+    eliminations = 0
+    while index < len(list):
+        if word == list[index]:
+            list[index] = ""
+            eliminations += 1
+        index += 1
+    return eliminations
--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,19 @@
 def enumerate_list(list):
-    return list
+    new_list = []
+    index_new = 0
+    for current_elem in list:
+        if current_elem != "":
+            new_list.append(f"{index_new}. {current_elem}")
+            index_new += 1
+    return new_list
+
 
 
 def enumerate_backwards(list):
-    return list
+    new_list = []
+    index_new = 0
+    for current_elem in list:
+        if current_elem != "":
+            new_list.append(f"{index_new}. {current_elem[::-1]}")
+            index_new += 1
+    return new_list
